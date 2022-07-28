120b75d287d6314ad5f805b64bd1926762bd097c
s-bolitho <sambolitho4@gmail.com>
2022-07-28 17:54:53 -0500

    Final commit

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -23,7 +23,7 @@ console.log(`${firstItem}`)
 
 let arr = [10,20,30,40,50,60]
 
-let lastItemRemoved = arr.pop()
+arr.pop()
 
 console.log(`${arr}`)
 
@@ -39,11 +39,17 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 
 //Code Here
 
+let family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
+
+for (let i = 0; i < family.length; i++){
+    console.log(family[i])
+}
+
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
-var nums = [1,2,3,6,22,98,45,23,22,12];
+let nums = [1,2,3,6,22,98,45,23,22,12];
 let evensArr = []
 // Do not edit the code above.
 
@@ -53,6 +59,16 @@ let evensArr = []
 
 //Code Here
 
+let nums = [1,2,3,6,22,98,45,23,22,12];
+let evensArr = []
+
+for (let i = 0; i < nums.length; i++){
+  if (i % 2 === 0) {
+    evensArr.push(nums[i])
+  }
+}
+
+console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
@@ -66,6 +82,24 @@ var score = 74
 */
 
 //Code Here
+
+var score = 74
+
+if (score >= 90){
+  console.log('A')
+}
+else if (score >= 80 && score <= 89){
+  console.log('B')
+}
+else if (score >= 70 && score <= 79){
+  console.log('C')
+}
+else if (score >= 60 && score <= 69){
+  console.log('D')
+}
+else {
+  console.log('F')
+}
 
 
 ////////// Intermediate Problems //////////
@@ -82,6 +116,10 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+var myFavoriteNumbers = [4,8,12,16,20,24];
+let someNum = myFavoriteNumbers[5];
+
+console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
@@ -90,6 +128,15 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+var myFavoriteNumbers = [4,8,12,16,20,24];
+let someNum = myFavoriteNumbers[5]
+
+if (myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in the array')
+}
+else {
+  someNum = myFavoriteNumbers.splice(7, 1, myFavoriteNumbers[7])
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -101,6 +148,13 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+let listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+for (let i = 0; i < listOfNumbers.length; i++) {
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(`${listOfNumbers[i]} is divisible by 3`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -112,6 +166,15 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+
+var letters = ['A', 'B', 'C', 'D', 'E'];
+
+for (i = 4; i >= 0; i--){
+  console.log(`${letters[i]}`)
+}
+
+
+
 
 
 ////////// Advanced Problems //////////
@@ -135,7 +198,25 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
+let letterGrade = 'B'
 
+switch (letterGrade){
+  case 'A':
+    console.log('The student is doing excellently');
+    break;
+  case 'B':
+    console.log('The student is doing well')
+    break;
+  case 'C':
+    console.log('The student is doing alright')
+    break;
+  case 'D':
+    console.log('The student is not doing very well')
+    break;
+  case 'F':
+    console.log('The student is failing')
+    break;
+}
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
@@ -162,3 +243,18 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for (i = 1; i <= 100; i++){
+  if (i % 15 === 0){
+    console.log("DevMountain")
+  }
+  else if (i % 3 === 0){
+    console.log("Dev")
+  }
+  else if ( i % 5 === 0){
+    console.log("Mountain")
+  }
+  else{
+    console.log(i)
+  }
+}
